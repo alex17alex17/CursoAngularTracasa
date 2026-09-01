6c7d52adf683023d5836bc1edba98c042e8a0fff
Alex <asarango@itracasa.es>
2026-09-01 12:53:46 +0200

    Add Angular Material theming and refactor styles across components; implement responsive design and improve UI consistency

--- src/app/features/Expedientes/Components/busqueda-expedientes/busqueda-expedientes.ts
+++ src/app/features/Expedientes/Components/busqueda-expedientes/busqueda-expedientes.ts
@@ -1,12 +1,25 @@
 import { Component, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { FiltrosBusqueda } from '../../models/expediente-busqueda';
 import { Expediente } from '../../models/expediente-interface';
 import { BusquedaExpedientesPaginacion } from '../busqueda-expedientes-paginacion/busqueda-expedientes-paginacion';
 
 @Component({
   selector: 'app-busqueda-expedientes',
-  imports: [FormsModule, BusquedaExpedientesPaginacion],
+  imports: [
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    BusquedaExpedientesPaginacion,
+  ],
   templateUrl: './busqueda-expedientes.html',
   styleUrl: './busqueda-expedientes.css',
 })
@@ -17,6 +30,14 @@ export class BusquedaExpedientes {
   filtroTabla = output<FiltrosBusqueda>();
   cambioPagina = output<number>();
   expedienteSeleccionado = output<Expediente>();
+  readonly displayedColumns = [
+    'numero',
+    'titulo',
+    'estado',
+    'prioridad',
+    'fechaAlta',
+    'fechaVencimiento',
+  ];
 
   filtros: FiltrosBusqueda = {
     texto: '',
